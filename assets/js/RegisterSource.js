85c1cac438a62639c713611b714a4d9040378ab7
Joseg_17 <joseangelrojas.2221@gmail.com>
2021-04-02 12:27:45 -0400

    fix: functionality register-page and some perfs

--- assets/js/RegisterSource.js
+++ assets/js/RegisterSource.js
@@ -1,11 +1,12 @@
 /**
- * @function {line 8} used to validate input fields
- * @
+ * @function {line 9} used to validate input fields
  */
      let joinButton = document.querySelector('#join');
     
 joinButton.setAttribute('disabled','');
-confirmation = [];
+
+confirmation = [false,false,false,false,false,false];
+
 document.querySelectorAll('input').forEach((input,index) =>  input.addEventListener('input', () => {
     Password1 = document.getElementById('password-user').value;
     Password2 = document.getElementById('password-user-repeat').value;
@@ -15,12 +16,6 @@ document.querySelectorAll('input').forEach((input,index) =>  input.addEventListe
     }
 
     if(confirmation.lenght = 6 && confirmation[index] === true && Password1 === Password2){
-        if(Password1!= Password2){
-            document.getElementById('error').classList.add('displayed') 
-        }
-        else{
-            document.getElementById('error').classList.remove('displayed');
-        }
         joinButton.classList.add('button');  
         joinButton.removeAttribute('disabled');
     }
@@ -28,5 +23,33 @@ document.querySelectorAll('input').forEach((input,index) =>  input.addEventListe
         joinButton.classList.remove('button');
         joinButton.setAttribute('disabled','');
     }
+
+    if(Password1!= Password2){
+            document.getElementById('error').classList.add('displayed') 
+    }
+    else{
+            document.getElementById('error').classList.remove('displayed');
+    }
 }))
+
+//let urlAction = document.getElementsByName('userForm')[0].method.toString();
+//
+//class tokenData {
+//constructor(token){
+//     this.token = token
+//    }
+//
+//    display(){
+//        console.log(this.token)
+//    }
+//}
+//
+//fetch(urlAction)
+//    .then(response => response.json())
+//    .then(data => {
+//        let userToken = new tokenData(data.token); 
+//        let userTokenParse = userToken.token.toString();
+//        localStorage.setItem('token', userTokenParse);
+//    })
+//    .catch(err => console.log(err))
 
