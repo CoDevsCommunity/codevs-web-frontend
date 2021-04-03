19e702b92ff42c99f6dfbc13e50a9ed75a99dfd0
Joseg_17 <joseangelrojas.2221@gmail.com>
2021-04-03 16:09:28 -0400

    fix: little changes in the form functionality

--- assets/js/RegisterSource.js
+++ assets/js/RegisterSource.js
@@ -1,26 +1,23 @@
 /**
- * @function {line 8} used to validate input fields
- * @
+ * @function {line 9} validate the input fields when triggers 
  */
-     let joinButton = document.querySelector('#join');
-    
-joinButton.setAttribute('disabled','');
-confirmation = [];
-document.querySelectorAll('input').forEach((input,index) =>  input.addEventListener('input', () => {
-    Password1 = document.getElementById('password-user').value;
-    Password2 = document.getElementById('password-user-repeat').value;
+let joinButton = document.querySelector('#join');
+    confirmation = [];
 
-    if(input.validity.valid === true && Password1 != "" && Password2 != ""){
-        confirmation[index] = input.validity.valid;
+joinButton.setAttribute('disabled','');
+
+document.querySelectorAll('input').forEach((input,index) =>   
+   
+    input.addEventListener('input', () => {
+
+    if(input.validity.valid === true){
+        confirmation[index] = true;
+    }
+    else{
+        confirmation[index] = false;
     }
 
-    if(confirmation.lenght = 6 && confirmation[index] === true && Password1 === Password2){
-        if(Password1!= Password2){
-            document.getElementById('error').classList.add('displayed') 
-        }
-        else{
-            document.getElementById('error').classList.remove('displayed');
-        }
+    if(reconf.lenght = 6 && confirmation[index] === true){
         joinButton.classList.add('button');  
         joinButton.removeAttribute('disabled');
     }
@@ -28,5 +25,16 @@ document.querySelectorAll('input').forEach((input,index) =>  input.addEventListe
         joinButton.classList.remove('button');
         joinButton.setAttribute('disabled','');
     }
-}))
+}));
 
+document.querySelectorAll('input[type=password]').forEach((input,index) => input.addEventListener('input', () => {
+    Password1 = document.getElementById('password-user').value;
+    Password2 = document.getElementById('password-user-repeat').value;
+
+    if(Password1!= Password2){
+        document.getElementById('error').classList.add('displayed');
+    }
+    else{
+        document.getElementById('error').classList.remove('displayed');
+    }
+}))
--- assets/js/clearButton.js
+++ assets/js/clearButton.js
@@ -2,9 +2,9 @@
  * this function clean the inputs fields if the user press "clean" button
  * @param {foo var} e - Used to prevent the default action
  */
-    let ButtonClear = document.querySelector('#clear'); 
+    ButtonClear = document.querySelector('#clear'); 
 
-    let form = document.getElementsByName('userForm')[0];
+    form = document.getElementsByName('userForm')[0];
 
 function clear(e) {	
 	 e.preventDefault();
